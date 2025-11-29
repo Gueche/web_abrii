1166c190bf107abed8878c9a3f3d638a25467eab
Gueche <153389905+Gueche@users.noreply.github.com>
2025-11-29 09:19:06 +0100

    Add files via upload

---
+++ 1_helloworld.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("Welcome to this course.")
+
+# Ceci est un commentaire sur une ligne
+print("Bonjour Minteligencia!") # Encore un commentaire
+#print("On peut aussi commenter le code.")
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# web_abrii
-Python Analyse 
+# Formation-Python
